7b937067a79cf3cd77fa6ec2a0693e0c6fdcd1be
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-08-17 01:11:27 +0430

    add credentioal info

--- createDevice/main.go
+++ createDevice/main.go
@@ -4,6 +4,7 @@ import (
 	"AWS_challenge1/createDevice/serviceHandler"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
@@ -15,8 +16,14 @@ var dynamoDB *dynamodb.DynamoDB
 
 func init() {
 	region := os.Getenv("AWS_REGION")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	secretKey := os.Getenv("SECRET_KEY")
+	credential := credentials.NewStaticCredentials(accessToken, secretKey, "")
 	awsSession, err := session.NewSession(&aws.Config{
-		Region: aws.String(region)},
+		Region:      aws.String(region),
+		Credentials: credential,
+	},
+
 	)
 	if err != nil {
 		log.Println(err)
